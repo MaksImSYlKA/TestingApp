898cd84ead39d8f20cab4efe003c9b1fc2a424da
Maxim Sylka <msylka@me.com>
2024-01-06 18:01:23 +0300

    Add few controls and customization to it

--- TestingApp/ViewController.swift
+++ TestingApp/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        segmentedControl.insertSegment(
+            withTitle: "Third",
+            at: 2,
+            animated: false
+        )
+        setupSlider()
+        setupMainLabel()
     }
-
+    @IBAction func sliderAction() {
+        mainLabel.text = slider.value.formatted()
+        view.backgroundColor = view.backgroundColor?.withAlphaComponent(CGFloat(slider.value))
+    }
+    @IBAction func segmentedControlAction(){
+        switch segmentedControl.selectedSegmentIndex {
+            case 0:
+                mainLabel.text = "Th first segment is selected"
+            case 1:
+                mainLabel.text = "Th second segment is selected"
+            case 2:
+                                mainLabel.text = "Th third segment is selected"
+            default:
+                mainLabel.text = slider.value.formatted()
+            
+                
+        }
+    }
+    private func setupSlider(){
+        slider.value = 1
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .green
+        
+    }
+    private func setupMainLabel(){
+        mainLabel.text = slider.value.formatted()
+        mainLabel.font = .systemFont(ofSize: 35)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+    }
 
 }
 
